b4bbea98c3f37fafbb40dfa35c8099ac7586338c
Alex <alexgascon.93@gmail.com>
2017-03-05 02:42:28 +0100

    Updated main.py
    Updated main.py so it now indicates if the weather is good or not

--- src/main.py
+++ src/main.py
@@ -6,21 +6,28 @@ import telebot
 import os
 from flask import Flask, request
 
-from weather import get_current_weather_in_location
+from weather import get_current_weather_in_location, is_bad_weather
 
 # Creating the bot
 TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN') # Token previously stored in an environment var
 bot = telebot.TeleBot(TOKEN)
 
-# Creating the server
-server = Flask(__name__)
-
 
 # The decorator (@bot.message_handler) indicates the type of messages that will activate this function
 @bot.message_handler(content_types=['location'])
-def get_current_weather_from_location_message(message):
+def answer_if_I_have_to_worry_from_location_message(message):
+
+    # Getting the current weather
     weather = get_current_weather_in_location(message)
-    bot.reply_to(message, weather)
+
+    # Checking if it's raining/snowing/thunderstorming/etc
+    I_need_to_worry =  is_bad_weather(weather)
+
+    if I_need_to_worry:
+        bot.reply_to(message, 'Males notícies... Si tens roba estesa, ja cal que la llaves altra volta')
+    else:
+        bot.reply_to(message, 'Pots estar tranquil, que fa bon temps!')
+
 
 @bot.message_handler(regexp='^ping$')
 def test_that_bot_works(message):
