e3d64d9b0088dd881d2b60276d836b3dd6eb50af
Alex <alexgascon.93@gmail.com>
2017-03-19 20:25:31 +0100

    Set "/lavadora" command

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 import telebot
 import os
 
-from weather import get_current_weather_in_location, is_bad_weather
+from weather import get_current_weather_in_location, is_bad_weather, get_5day_forecast_in_location
 
 # Creating the bot
 TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN') # Token previously stored in an environment var
@@ -19,11 +19,24 @@ def answer_if_I_have_to_worry_from_location_message(message):
     # Checking if it's raining/snowing/thunderstorming/etc
     I_need_to_worry = [is_bad_weather(weather) for weather in weathers]
 
-
     if True in I_need_to_worry:
         bot.reply_to(message, 'Males notícies... Si tens roba estesa, ja cal que la llaves altra volta')
     else:
         bot.reply_to(message, 'Pots estar tranquil, que fa bon temps!')
+
+@bot.message_handler(commands=['lavadora'])
+def check_5day_forecast_from_location(message):
+
+    weathers = get_5day_forecast_in_location(message)
+
+    # Checking if it's raining/snowing/thunderstorming/etc
+    I_need_to_worry = [is_bad_weather(weather) for weather in weathers]
+
+    if True in I_need_to_worry:
+        bot.reply_to(message, 'Et recomane mirar bé el temps abans, ha de ploure en els pròxims 5 dies')
+    else:
+        bot.reply_to(message, 'Pots estar tranquil, que fa bon temps!')
+
 
 
 @bot.message_handler(regexp='^ping$')
