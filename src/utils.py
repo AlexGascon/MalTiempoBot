df79968e12f93003cf72f789950f719046e08806
Alex <alexgascon.93@gmail.com>
2017-03-23 21:36:01 +0100

    Revert commit b70e4c325d7a56c54fef4740f4910bcc94564c4d

--- src/utils.py
+++ src/utils.py
@@ -1,26 +1,9 @@
 import os
-import psycopg2
 
 
-"""API SECTION"""
 def get_OpenWeatherAPI_token():
     # Setting the API Key
     TOKEN = os.environ.get('OPENWEATHERMAP_TEST_TOKEN')
     # TOKEN = os.environ.get('OPENWEATHERMAP_PRODUCTION_TOKEN')
 
     return TOKEN
-
-"""DATABASE SECTION"""
-def get_db_cursor():
-    """
-    Connects to the database and returns the cursor to use to interact with it
-    """
-    DB_URI = os.environ.get('DATABASE_URL')
-    
-
-    pass
-
-def store_user_location():
-    """
-
-    """
