8d4baabb5d2c61d11e0681d95f39c7d356162c73
Alex <alexgascon.93@gmail.com>
2017-05-07 00:27:22 +0200

    Change in weather strings

--- src/constants.py
+++ src/constants.py
@@ -39,7 +39,7 @@ SECTION: ANSWER SENTENCES
 TODAY_NO_WORRY_ENG = "Don't worry bro, the sun will shine today!"
 TODAY_WORRY_ENG = "Bad news, today it's rain day..."
 FORECAST_NO_WORRY_ENG = "Don't worry pal! The sun is shining and will keep doing it!"
-FORECAST_WORRY_ENG = "You should with more detail to the weather, it will rain within 3 days"
+FORECAST_WORRY_ENG = "You should with more detail to the weather, it will rain within 5 days"
 LOCATION_STORED_CORRECTLY_ENG = "Location correctly updated!"
 LOCATION_NOT_STORED_CORRECTLY_ENG = "Impossible to update location. Please try again later"
 HELP_ENG = "/umbrella - Tells if the weather will be bad in the following 24 hours\n" \
@@ -47,8 +47,8 @@ HELP_ENG = "/umbrella - Tells if the weather will be bad in the following 24 hou
 
 TODAY_NO_WORRY_VAL = "Pots estar tranquil, que fa bon temps!"
 TODAY_WORRY_VAL = "Males notícies... Si tens roba estesa, ja cal que la llaves altra volta"
-FORECAST_NO_WORRY_VAL = "Pots estar tranquil, que fa i farà bon temps els pròxims 3 dies!"
-FORECAST_WORRY_VAL = "Et recomane mirar bé el temps abans, ha de ploure en els pròxims 3 dies"
+FORECAST_NO_WORRY_VAL = "Pots estar tranquil, que fa i farà bon temps els pròxims 5 dies!"
+FORECAST_WORRY_VAL = "Et recomane mirar bé el temps abans, ha de ploure en els pròxims 5 dies"
 LOCATION_STORED_CORRECTLY_VAL = "Localització actualitzada correctament!"
 LOCATION_NOT_STORED_CORRECTLY_VAL = "No s'ha pogut actualitzar la localització. Per favor, intenta-ho de nou en un altre moment"
 HELP_VAL = "/paraguas - Indica si hace mal tiempo en este momento\n" \
