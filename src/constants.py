dad646429a02d71b9b33f52ba1b46620149712cf
Alex <alexgascon.93@gmail.com>
2017-05-18 23:50:42 +0200

    Creating introduction strings

--- src/constants.py
+++ src/constants.py
@@ -36,6 +36,17 @@ WEATHER_GROUP_GOOD_WEATHER = (
 """
 SECTION: ANSWER SENTENCES
 """
+INTRODUCTION_ENG = """Hi human! I'm @BadWeatherBot, the bot that will help you know if the weather will be bad in a \
+                   specific moment. You have two different options: 
+                   
+                   If you need to know if you should take an umbrella today when going out, you can use the /umbrella \
+                   command. It will indicate if it will rain, snow or anything similar in the following 24 hours.
+                   
+                   On the other hand, if you're planning to wash your clothes, then the /washingmachine command is for \
+                   you! With it, you'll now if there will be any bad weather condition within 3 days, so you can decide \
+                   if you should hang out the clothes or not.
+                   
+                   But first, in order to do this, I'll need to know your location. Would you mind sending it to me? """
 TODAY_NO_WORRY_ENG = "Don't worry bro, the sun will shine today!"
 TODAY_WORRY_ENG = "Bad news, today it's rain day..."
 FORECAST_NO_WORRY_ENG = "Don't worry pal! The sun is shining and will keep doing it!"
@@ -45,6 +56,18 @@ LOCATION_NOT_STORED_CORRECTLY_ENG = "Impossible to update location. Please try a
 HELP_ENG = "/umbrella - Tells if the weather will be bad in the following 24 hours\n" \
     + "/washingmachine - Tells if the weather will be bad within 3 days"
 
+
+INTRODUCTION_VAL = """Bon dia company! Soc @MalTiempoBot, el bot que et permetrà saber si farà mal temps pròximament. \
+Disposes de dos opcions: 
+
+Si necessites saber si deuries agafar paraigües hui per a ixir de casa, necessites el comando /paraguas. T'indicarà si \
+durant les pròximes 24h plourà, nevarà, o qualsevol condició meteorològica similar.
+ 
+Si el que necessites saber és si pots estendre tranquil o no, has d'utilitzar /lavadora. Amb este comando t'indicaré si \
+ha de fer mal temps en algun moment durant els pròxims 3 dies, per a que no t'hages de preocupar de tindre que rentar \
+dos voltes seguides la mateixa roba.
+
+Però abans que res, per a poder funcionar correctament, necessite saber la teua ubicació. T'importa enviar-me-la?"""
 TODAY_NO_WORRY_VAL = "Pots estar tranquil, que fa bon temps!"
 TODAY_WORRY_VAL = "Males notícies... Si tens roba estesa, ja cal que la llaves altra volta"
 FORECAST_NO_WORRY_VAL = "Pots estar tranquil, que fa i farà bon temps els pròxims 3 dies!"
