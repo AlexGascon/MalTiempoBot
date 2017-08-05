6d6f7c57747fe30f061b9f970f706a00c606d36a
Alex <alexgascon.93@gmail.com>
2017-08-05 13:24:51 +0200

    Creating Forecast model

--- src/models.py
+++ src/models.py
@@ -28,3 +28,31 @@ class Weather:
     def is_bad(self):
         """Return a boolean indicating if the weather is bad (rain, snow...)"""
         return self.id not in WEATHER_GROUP_GOOD_WEATHER
+
+
+class Forecast:
+    """Collection of Weather objects
+
+    Parameters:
+        weathers -- Weather conditions included in the forecast
+    """
+
+    def __init__(self, api_response):
+        """Constructor. Deserialize the JSON text and parse the parameters
+
+        Arguments:
+        api_response -- the response returned after calling OpenWeatherMap API
+        """
+
+        self.weathers = []
+
+        # Deserializing the JSON text
+        json_text = json.loads(api_response)
+
+        # Key to obtain the weather section of the JSON response
+        weather_list = json_text['list']
+
+        for weather_info in weather_list:
+            self.weathers.append(Weather(weather_info))
+
+
