60e65011b56349b8236462d3cc5c93bd103fdc6d
Alex <alexgascon.93@gmail.com>
2017-08-05 12:53:08 +0200

    Added is_bad method

--- src/models.py
+++ src/models.py
@@ -1,11 +1,13 @@
 import json
 
+from constants import WEATHER_GROUP_GOOD_WEATHER
+
 
 class Weather:
     """Represents the weather information obtained when calling the OpenWeatherMap API"""
 
     def __init__(self, api_response):
-
+        """Constructor. Deserializes the JSON text and parses the parameters"""
         # Deserializing the JSON text
         json_text = json.loads(api_response)
 
@@ -18,3 +20,8 @@ class Weather:
         self.main = weather_section['main']
         self.description = weather_section['description']
         self.icon = weather_section['icon']
+
+    def is_bad(self):
+        """Method that returns a boolean indicating if the weather is bad (rain, snow, thunderstorm...)"""
+        return self.id not in WEATHER_GROUP_GOOD_WEATHER
+    
