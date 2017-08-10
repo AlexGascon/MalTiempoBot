ad93814a39732c94187bc0c39e7aee4430799fe7
Alex <alexgascon.93@gmail.com>
2017-08-10 17:39:16 +0200

    Fixing Weather init method

--- src/models.py
+++ src/models.py
@@ -13,24 +13,34 @@ class Weather:
         icon -- weather condition ID (currently unused)
     """
 
-    def __init__(self, api_response):
+    def __init__(self, api_response=None, weather_info=None):
         """Constructor. Deserialize the JSON text and parse the parameters
 
+        The Weather object may be created either independently or as a part of a Forecast object. In the second case,
+        the API response will have already been deserialized, and the input will be a Python dict.
+
         Arguments:
-        api_response -- the response returned after calling OpenWeatherMap API
+        api_response -- the JSON response returned after calling OpenWeatherMap API
+        weather_info -- dict that contains the api_response after deserializing the JSON
         """
-        # Deserializing the JSON text
-        json_text = json.loads(api_response)
+
+        if weather_info is None and api_response is None:
+            raise ValueError
+
+        elif api_response is not None:
+            # Deserializing the JSON text
+            weather_info = json.loads(api_response)
 
         # Key to obtain the weather section of the JSON response
         weather_key = 'weather'
-        weather_section = json_text[weather_key]
+        weather_section = weather_info[weather_key][0]
 
         # Assigning the parameters
         self.id = weather_section['id']
         self.main = weather_section['main']
         self.description = weather_section['description']
         self.icon = weather_section['icon']
+
 
     def is_bad(self):
         """Return a boolean indicating if the weather is bad (rain, snow...)"""
