7c496bbbed840e6c2688caa06da0aac7cb890dcc
Alex <alexgascon.93@gmail.com>
2017-10-21 23:47:41 +0200

    Refactoring tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,26 +5,32 @@ from src.models import Weather
 
 
 class TestWeather(unittest.TestCase):
+    """Testing the Weather model of the project"""
+
     def setUp(self):
         self.json_api_response = open("./fixtures/weather.json").read()
         self.dict_api_response = json.loads(self.json_api_response)
 
     def test_json_constructor(self):
+        """Weather objects can be correctly created from OWM api response in JSON format"""
+
         test_weather = Weather(api_response=self.json_api_response)
 
-        assert test_weather.id == 300
-        assert test_weather.main == 'Drizzle'
-        assert test_weather.description == 'light intensity drizzle'
-        assert test_weather.icon == '09d'
+        self.assertEqual(test_weather.id, 300)
+        self.assertEqual(test_weather.main, 'Drizzle')
+        self.assertEqual(test_weather.description, 'light intensity drizzle')
+        self.assertEqual(test_weather.icon, '09d')
 
     def test_dict_constructor(self):
+        """Weather objects can be correctly created from a dict containing OWM's API response"""
+
         test_weather = Weather(weather_info=self.dict_api_response)
 
-        assert test_weather.id == 300
-        assert test_weather.main == 'Drizzle'
-        assert test_weather.description == 'light intensity drizzle'
-        assert test_weather.icon == '09d'
+        self.assertEqual(test_weather.id, 300)
+        self.assertEqual(test_weather.main, 'Drizzle')
+        self.assertEqual(test_weather.description, 'light intensity drizzle')
+        self.assertEqual(test_weather.icon, '09d')
 
 
 if __name__ == '__main__':
-    unittest.__main__() 
+    unittest.__main__()
