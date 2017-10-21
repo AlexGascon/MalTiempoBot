d5117aeb6c525da5f6bc08970e933bc3002d13b2
Alex <alexgascon.93@gmail.com>
2017-10-21 23:36:38 +0200

    Revert "Refactoring previous tests"
    This reverts commit 2e8323910011e110c727f3a33800b430cd54f8e3.

--- test/test_models.py
+++ test/test_models.py
@@ -5,14 +5,12 @@ from src.models import Weather
 
 
 class TestWeather(unittest.TestCase):
-    """Testing the Weather model of the project"""
 
     def setUp(self):
         self.json_api_response = open("./fixtures/weather.json").read()
         self.dict_api_response = json.loads(self.json_api_response)
 
     def test_json_constructor(self):
-        """Weather objects can be correctly created from OWM api response in JSON format"""
         test_weather = Weather(api_response=self.json_api_response)
 
         assert test_weather.id == 300
@@ -21,7 +19,6 @@ class TestWeather(unittest.TestCase):
         assert test_weather.icon == '09d'
 
     def test_dict_constructor(self):
-        """Weather objects can be correctly created from a dict containing OWM's API response"""
         test_weather = Weather(weather_info=self.dict_api_response)
 
         assert test_weather.id == 300
